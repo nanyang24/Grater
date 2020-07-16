6ab411ba6a51865fb51a30d9bd4f38a59b0f3a3d
NanYang <nanyang24@hotmail.com>
2020-07-16 22:12:32 +0800

    feat: Decimal with Underscore

--- src/tokenizer/numeric.ts
+++ src/tokenizer/numeric.ts
@@ -1,7 +1,7 @@
 import { IParserState } from '../parser/type';
 import { Token } from './token';
 import { forwardChar } from './utils';
-import { CharTypes, CharSymbol } from './charClassifier';
+import { CharTypes, CharSymbol, Chars } from './charClassifier';
 
 /*
  * https://tc39.es/ecma262/index.html#prod-NumericLiteral
@@ -14,15 +14,40 @@ import { CharTypes, CharSymbol } from './charClassifier';
 export const scanNumber = (parser: IParserState): Token => {
   let char = parser.currentChar;
   let value = '';
-  const start = parser.index;
+  let start = parser.index;
+  let underscoreError = false;
 
   // Decimal Number
-  while (CharTypes[char] & CharSymbol.Decimal) {
-    char = forwardChar(parser);
+  while (CharTypes[char] & (CharSymbol.Decimal | CharSymbol.Underscore)) {
+    // Note: The first char won't be the '_'，Because it's a Identifier in that case.
+    // This should be like '123', '1_23', '123_'
+    // '_'
+    if (char === Chars.Underscore) {
+      // Save the character index before the underscore
+      const { index: indexBeforeUnderscore } = parser;
+
+      char = forwardChar(parser);
+      // Cannot contain two consecutive underscores
+      if (char === Chars.Underscore) {
+        // error
+      }
+      underscoreError = true;
+      // skip '_'
+      value += parser.source.substring(start, indexBeforeUnderscore);
+      start = parser.index;
+
+      continue;
+    }
+    underscoreError = false;
+    char = forwardChar(parser); // skip
   }
 
-  value += parser.source.substring(start, parser.index);
-  parser.tokenValue = +value;
+  if (underscoreError) {
+    // error
+    return -1;
+  }
+
+  parser.tokenValue = value + parser.source.substring(start, parser.index);
 
   return Token.NumericLiteral;
 };
